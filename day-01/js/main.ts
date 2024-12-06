c3ccf79428c4e7e63411b71b28311665c11cc66b
Sergey Kasmy <sergey.kasmy@gmail.com>
2024-12-06 23:30:08 +0100

    Solve day 1 part 2 in js

--- day-01/js/main.ts
+++ day-01/js/main.ts
@@ -14,13 +14,38 @@ for (const line of lines) {
 	rightList.push(Number(sides[1]));
 }
 
-leftList.sort((a, b) => a - b);
-rightList.sort((a, b) => a - b);
+console.log("The total sum of the differences is", sumOfDifferences(leftList, rightList));
+console.log("The total sum of the similarities is", sumOfSimilarities(leftList, rightList));
 
-let differences = 0;
+// part 1
+function sumOfDifferences(leftList: number[], rightList: number[]): number {
+	leftList.sort((a, b) => a - b);
+	rightList.sort((a, b) => a - b);
 
-for (let i = 0; i < leftList.length; i++) {
-	differences += Math.abs(leftList[i] - rightList[i]);
+	let differences = 0;
+
+	for (let i = 0; i < leftList.length; i++) {
+		differences += Math.abs(leftList[i] - rightList[i]);
+	}
+
+	return differences;
 }
 
-console.log("The total sum of the differences is", differences);
+// part 2
+function sumOfSimilarities(leftList: number[], rightList: number[]): number {
+	// set of all unique IDs in the leftList
+	const leftSet = new Set(leftList);
+
+	// map of all unique IDs with the key = the ID itself and the value = the number of times it appeared
+	const rightMap = new Map<number, number>();
+	for (const x of rightList) {
+		rightMap.set(x, (rightMap.get(x) ?? 0) + 1);
+	}
+
+	let similarities = 0;
+	for (const id of leftSet) {
+		similarities += id * (rightMap.get(id) ?? 0);
+	}
+
+	return similarities;
+}
