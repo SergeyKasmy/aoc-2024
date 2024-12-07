ae2dc227c50087e6fc8a1ed6f365f128bd233290
Sergey Kasmy <sergey.kasmy@gmail.com>
2024-12-08 00:55:01 +0100

    Move run_solution function to a separate script
    which can later be shared between the days

---
+++ util/run_solution.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+run_solution() {
+	name="$1"
+	dir="$2"
+	build_command="$3"
+	run_command="$4"
+
+	echo "-- $name"
+
+	cd "./$dir"
+
+	if [ -n "$build_command" ]; then
+		echo " > $build_command"
+		eval "$build_command"
+	fi
+
+	echo " > $run_command"
+	eval "$run_command"
+
+	echo
+	cd ..
+}
