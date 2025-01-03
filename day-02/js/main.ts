6f90554686e41a311add33e57b4069d89c447c5b
Sergey Kasmy <sergey.kasmy@gmail.com>
2025-01-03 21:24:41 +0100

    Solve day 2 part 2 in JS

--- day-02/js/main.ts
+++ day-02/js/main.ts
@@ -1,38 +1,71 @@
+const DEBUG = false;
+const debug = DEBUG
+	? (...args: unknown[]) => {
+		console.log("DEBUG:", ...args);
+	}
+	: () => {};
+
 const input = Deno.readTextFileSync(Deno.args[0]);
-const reports = input.trim().split("\n").map((report) => report.split(" ").map((level) => Number(level)));
-const safeReports = reports.reduce((safeReports, report) => safeReports + Number(isReportSafe(report)), 0);
+const reports = input.trim().split("\n").map((report) =>
+	report.split(" ").map((level) => Number(level))
+);
+
+debug("Loaded", reports.length, "reports");
+
+const safeReports = reports.reduce(
+	(safeReports, report) => safeReports + Number(isReportSafe(report, false)),
+	0,
+);
+const safeReportsSkipped = reports.reduce(
+	(safeReports, report) => safeReports + Number(isReportSafe(report, true)),
+	0,
+);
 
 console.log("Total number of safe reports is:", safeReports);
+console.log(
+	"Total number of safe reports with 1 value skipped is:",
+	safeReportsSkipped,
+);
 
-function isReportSafe(report: number[]): boolean {
-	// console.log("Report is:", report);
+function isReportSafe(report: number[], canSkip: boolean): boolean {
+	debug("Report is:", report);
 
-	const isIncreasing = report[0] - report[1] < 0;
-	// console.log("isIncreasing:", isIncreasing);
+	if (isSequenceSafe(report)) {
+		return true;
+	}
 
-	for (let i = 1; i < report.length; i++) {
-		const delta = report[i - 1] - report[i];
-		// console.log("Delta of", report[i - 1], "and", report[i], "is", delta);
+	if (!canSkip) {
+		return false;
+	}
 
-		if (Math.abs(delta) > 3) {
-			// console.log("delta too big, not safe");
-			return false;
-		}
-
-		if (isIncreasing) {
-			// if delta is descreasing
-			if (delta >= 0) {
-				// console.log("delta is descreasing after initially increasing, not safe");
-				return false;
-			}
-		} else {
-			if (delta <= 0) {
-				// console.log("delta is inscreasing after initially descreasing, not safe");
-				return false;
-			}
+	for (let i = 0; i < report.length; i++) {
+		const newReport = report.slice(0, i).concat(report.slice(i + 1));
+		if (isSequenceSafe(newReport)) {
+			return true;
 		}
 	}
 
-	// console.log("All checks passed, it's safe:", report);
+	return false;
+}
+
+function isSequenceSafe(report: number[]): boolean {
+	let isIncreasing = null;
+
+	for (let i = 1; i < report.length; i++) {
+		const delta = report[i] - report[i - 1];
+
+		if (Math.abs(delta) > 3 || delta === 0) {
+			return false;
+		}
+
+		if (isIncreasing === null) {
+			isIncreasing = delta > 0;
+		} else if (isIncreasing && delta < 0) {
+			return false;
+		} else if (!isIncreasing && delta > 0) {
+			return false;
+		}
+	}
+
 	return true;
 }
