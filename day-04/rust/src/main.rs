6035fb5579948812cc34bca8735e44b7f4762ce5
Sergey Kasmy <sergey.kasmy@gmail.com>
2025-01-10 00:19:59 +0100

    Solve day 4 part 2 in Rust

--- day-04/rust/src/main.rs
+++ day-04/rust/src/main.rs
@@ -1,27 +1,41 @@
 use std::{env, fs};
 
-use directions::DIRECTIONS;
+use directions::{ALL_DIRECTIONS, DIAG_DIRECTIONS};
 
-type Direction = [(isize, isize); 3];
+type Direction<const N: usize> = [(isize, isize); N];
 
 #[rustfmt::skip]
 mod directions {
     use crate::Direction;
 
-    const RIGHT: Direction = [(0, 1), (0, 2), (0, 3)];
-    const LEFT: Direction  = [(0, -1), (0, -2), (0, -3)];
-    const UP: Direction    = [(-1, 0), (-2, 0), (-3, 0)];
-    const DOWN: Direction  = [(1, 0), (2, 0), (3, 0)];
+    // starting from 0, 0
+    const RIGHT: Direction<4> = [(0, 0), (0, 1), (0, 2), (0, 3)];
+    const LEFT: Direction<4>  = [(0, 0), (0, -1), (0, -2), (0, -3)];
+    const UP: Direction<4>    = [(0, 0), (-1, 0), (-2, 0), (-3, 0)];
+    const DOWN: Direction<4>  = [(0, 0), (1, 0), (2, 0), (3, 0)];
 
-    const fn merge(a: Direction, b: Direction) -> Direction {
+    // starting from the corner
+    const RIGHT_DOWN: Direction<3> = [(-1, -1), (0, 0), (1, 1)];
+    const RIGHT_UP: Direction<3>   = [(1, -1), (0, 0), (-1, 1)];
+
+    const fn merge(a: Direction<4>, b: Direction<4>) -> Direction<4> {
         [
             (a[0].0 + b[0].0, a[0].1 + b[0].1),
             (a[1].0 + b[1].0, a[1].1 + b[1].1),
             (a[2].0 + b[2].0, a[2].1 + b[2].1),
+            (a[3].0 + b[3].0, a[3].1 + b[3].1),
         ]
     }
 
-    pub const DIRECTIONS: &[Direction] = &[
+    const fn reverse(dir: Direction<3>) -> Direction<3> {
+        [
+            (dir[2].0, dir[2].1),
+            (dir[1].0, dir[1].1),
+            (dir[0].0, dir[0].1),
+        ]
+    }
+
+    pub const ALL_DIRECTIONS: &[Direction<4>] = &[
         RIGHT,
         LEFT,
         UP,
@@ -30,6 +44,13 @@ mod directions {
         merge(RIGHT, DOWN),
         merge(LEFT, UP),
         merge(LEFT, DOWN),
+    ];
+
+    pub const DIAG_DIRECTIONS: &[Direction<3>] = &[
+        RIGHT_UP,
+        RIGHT_DOWN,
+        reverse(RIGHT_UP),
+        reverse(RIGHT_DOWN),
     ];
 }
 
@@ -44,38 +65,60 @@ fn main() {
     .map(|line| line.chars().collect::<Vec<_>>())
     .collect::<Vec<_>>();
 
-    let mut xmas_num = 0;
-
-    for i in 0..input.len() {
-        for j in 0..input[0].len() {
-            if input[i][j] != 'X' {
-                continue;
-            }
-
-            xmas_num += xmas_num_from_current_pos(&input, (i, j));
-        }
-    }
+    let xmas_num = find_word_in_grid::<4, false>(&input, ['X', 'M', 'A', 'S'], ALL_DIRECTIONS);
+    let mas_num = find_word_in_grid::<3, true>(&input, ['M', 'A', 'S'], DIAG_DIRECTIONS);
 
     println!("XMAS'es found: {xmas_num}");
+    println!("MAS'es in an X found: {mas_num}");
 }
 
-/// Returns the number of XMAS'es that could be reached from the current position in the grid
-fn xmas_num_from_current_pos(grid: &Vec<Vec<char>>, pos: (usize, usize)) -> usize {
-    assert!(grid[pos.0][pos.1] == 'X');
+fn find_word_in_grid<const N: usize, const COUNT_2_FOR_1: bool>(
+    grid: &Vec<Vec<char>>,
+    word: [char; N],
+    directions: &[Direction<N>],
+) -> usize {
+    let mut word_num = 0;
 
-    // number of xmases originating from this X
-    let mut xmas_num = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let count = word_count_from_pos(&grid, word, directions, (i, j));
 
-    for direction in DIRECTIONS {
-        if get_by_offset(grid, pos, direction[0]).is_some_and(|c| c == 'M')
-            && get_by_offset(grid, pos, direction[1]).is_some_and(|c| c == 'A')
-            && get_by_offset(grid, pos, direction[2]).is_some_and(|c| c == 'S')
-        {
-            xmas_num += 1
+            word_num += if COUNT_2_FOR_1 {
+                if count == 2 {
+                    1
+                } else {
+                    0
+                }
+            } else {
+                count
+            };
         }
     }
 
-    xmas_num
+    word_num
+}
+
+/// Returns the number of the word that could be reached in the specified directions from the current position in the grid
+fn word_count_from_pos<const N: usize>(
+    grid: &Vec<Vec<char>>,
+    word: [char; N],
+    directions: &[Direction<N>],
+    pos: (usize, usize),
+) -> usize {
+    // number of words originating from this position
+    let mut word_num = 0;
+
+    'direction: for direction in directions {
+        for (idx, letter) in word.iter().copied().enumerate() {
+            if get_by_offset(grid, pos, direction[idx]).is_none_or(|c| c != letter) {
+                continue 'direction;
+            }
+        }
+
+        word_num += 1;
+    }
+
+    word_num
 }
 
 /// Returns the value of the element by the offset from a position
